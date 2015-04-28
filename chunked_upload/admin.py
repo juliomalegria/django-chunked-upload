3c83af4b6eeee0ac1dd2b13c27b2bb62e6e05770
Julio M Alegria <juliomalegria@gmail.com>
2015-04-28 11:06:06 -0700

    If model is abstract, don't register ModelAdmin

--- chunked_upload/admin.py
+++ chunked_upload/admin.py
@@ -3,12 +3,11 @@ from django.contrib import admin
 from .models import ChunkedUpload
 from .settings import ABSTRACT_MODEL
 
+
+class ChunkedUploadAdmin(admin.ModelAdmin):
+    list_display = ('upload_id', 'filename', 'user', 'status', 'created_on')
+    search_fields = ('filename',)
+    list_filter = ('status',)
+
 if not ABSTRACT_MODEL:  # If the model exists
-
-    class ChunkedUploadAdmin(admin.ModelAdmin):
-        list_display = ('upload_id', 'filename', 'user', 'status',
-                        'created_on')
-        search_fields = ('filename',)
-        list_filter = ('status',)
-
     admin.site.register(ChunkedUpload, ChunkedUploadAdmin)
