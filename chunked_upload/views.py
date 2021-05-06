8b1feb98294fd819813e59163a6da46d4a9bd848
Steve Recio <stevenrecio@gmail.com>
2021-05-06 17:48:43 -0400

    use filename tmp instead of empty file (https://github.com/juliomalegria/django-chunked-upload/issues/60)

--- chunked_upload/views.py
+++ chunked_upload/views.py
@@ -142,7 +142,7 @@ class ChunkedUploadView(ChunkedUploadBaseView):
         """
         chunked_upload = self.model(**attrs)
         # file starts empty
-        chunked_upload.file.save(name='', content=ContentFile(''), save=save)
+        chunked_upload.file.save(name='tmp', content=ContentFile(''), save=save)
         return chunked_upload
 
     def is_valid_chunked_upload(self, chunked_upload):
